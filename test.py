c8a9e9e85a1f081712dff03cfb590f606a5e87b6
Muneeb <muneebshahid5@gmail.com>
2023-03-15 10:37:31 +0100

    Added a new actions file

---
+++ test.py
@@ -0,0 +1,9 @@
+def test_func(x):
+    return x + 1
+
+
+def test_fun(x):
+    return x + 2
+
+
+# The first function is from commint abce3 and the second is from commit 1234. What is the difference between them? Give me a summary of the changes.
