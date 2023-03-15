103f540f10f73be8e718c1844ad1e538e77bc09b
Muneeb <muneebshahid5@gmail.com>
2023-03-15 10:50:12 +0100

    Add open ai prompt

--- test.py
+++ test.py
@@ -6,4 +6,12 @@ def test_fun(x):
     return x + 2
 
 
-# The first function is from commint abce3 and the second is from commit 1234. What is the difference between them? Give me a summary of the changes.
+# Commit 1 defines a function called test_func that takes in a parameter 'x' and returns the value of x plus 1.
+
+# Commit 2 defines a function called test_fun that takes in a parameter 'x' and returns the value of x plus 2.
+
+# The difference between the two commits is that the second commit changes the name of the function from 'test_func' to 'test_fun' and changes the return value from 'x + 1' to 'x + 2'.
+
+# Summarize the above text in one sentence
+
+# Commit 2 changed the name of the function from 'test_func' to 'test_fun' and the return value from 'x + 1' to 'x + 2'.
