e36857e31c7499aebe5fa97999a60c655c94344f
Muneeb <muneebshahid5@gmail.com>
2023-03-15 11:10:39 +0100

    Make changes

--- test.py
+++ test.py
@@ -4,14 +4,3 @@ def test_func(x):
 
 def test_fun(x):
     return x + 2
-
-
-# Commit 1 defines a function called test_func that takes in a parameter 'x' and returns the value of x plus 1.
-
-# Commit 2 defines a function called test_fun that takes in a parameter 'x' and returns the value of x plus 2.
-
-# The difference between the two commits is that the second commit changes the name of the function from 'test_func' to 'test_fun' and changes the return value from 'x + 1' to 'x + 2'.
-
-# Summarize the above text in one sentence
-
-# Commit 2 changed the name of the function from 'test_func' to 'test_fun' and the return value from 'x + 1' to 'x + 2'.
