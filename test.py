a5d4e492a479739048a64ebe4a66f0b1ff945472
Muneeb <muneebshahid5@gmail.com>
2023-03-15 12:07:26 +0100

    Add print dir

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+from glob import glob
+
+
 def test_func(x):
     return x + 1
 
 
-def test_fun(x):
-    return x + 2
+def test_print_dir_contents(dir_: str):
+    contents = glob(dir_ + "/*")
+    for content in contents:
+        print(content)
 
 
 def test_func3(x):
